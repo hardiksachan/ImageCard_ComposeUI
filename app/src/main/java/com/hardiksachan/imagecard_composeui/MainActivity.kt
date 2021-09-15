94451165b4a10bacae7bacaa6f1606e6ebc21a59
Hardik Sachan <sachanhardik@gmail.com>
2021-09-16 04:33:26 +0530

    Created the Image Card

--- app/src/main/java/com/hardiksachan/imagecard_composeui/MainActivity.kt
+++ app/src/main/java/com/hardiksachan/imagecard_composeui/MainActivity.kt
@@ -3,11 +3,26 @@ package com.hardiksachan.imagecard_composeui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.hardiksachan.imagecard_composeui.ui.theme.ImageCard_ComposeUITheme
 
 class MainActivity : ComponentActivity() {
@@ -15,9 +30,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ImageCard_ComposeUITheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                val painter = painterResource(id = R.drawable.baby_yoda)
+                val description = "Baby Yoda"
+                val title = "Baby Yoda is having fun"
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(0.5f)
+                        .padding(16.dp)
+                ) {
+                    ImageCard(
+                        painter = painter,
+                        contentDescription = description,
+                        title = title
+                    )
                 }
             }
         }
@@ -25,14 +51,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+            ) {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ImageCard_ComposeUITheme {
-        Greeting("Android")
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
+
+        }
     }
 }
